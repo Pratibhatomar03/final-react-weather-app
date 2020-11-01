25d2b2ec284ab2ff8c461e672ea4b47ea3266b77
Pratibha <pratibha.tomar13@gmail.com>
2020-11-01 11:05:55 +0100

    added bootstrap and weather file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
+      <div className="Container">
+        <Weather />
+        <p>
+          Open-source code on <a href="/">GitHub</a> and hosted on{" "}
+          <a href="/">Netlify {""}</a>
+          by <a href="/"> Pratibha Tomar</a>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Let make some prediction</h1>
+    </div>
+  );
+}
